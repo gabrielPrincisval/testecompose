8b4df3b9e9c232eb22b05d6aeb49cc034260f814
Gabriel Princisval <95586319+gabrielPrincisval@users.noreply.github.com>
2022-02-11 21:30:42 -0300

    teste

---
+++ database/seeders/ProdutoSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Produto;
+class ProdutoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Produto::create([
+            'nome'   =>'Teclado',
+            'Valor'  => 150
+        ]);
+
+        Produto::create([
+            'nome'   =>'Mouse',
+            'Valor'  => 250
+        ]);
+
+
+        Produto::create([
+            'nome'   =>'Gabinete',
+            'Valor'  => 350
+        ]);
+    }
+}
