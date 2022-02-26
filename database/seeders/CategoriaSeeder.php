6ecadf94f7f74c0663b8ef1b18fdf3b214bcd780
Gabriel Princisval <95586319+gabrielPrincisval@users.noreply.github.com>
2022-02-25 21:53:11 -0300

    Create CategoriaSeeder.php

---
+++ database/seeders/CategoriaSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\Categoria;
+use Illuminate\Database\Seeder;
+
+class CategoriaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Categoria::create([
+            'nome' => 'Perifericos Gamers'
+        ]);
+            
+        Categoria::create([
+
+            'nome' => 'Perifericos Para Devs'
+            
+        ]);
+    
+    }
+
+}
